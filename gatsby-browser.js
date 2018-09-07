f28ba8fdde7bff3865624256969c89f45d895825
Manuel Mauky <manuel.mauky@saxsys.de>
2018-09-07 19:04:28 +0200

    fix for scrolling issues for anchors

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,6 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+const { anchorate } = require("anchorate")
 
- // You can delete this file if you're not using it
+
+exports.onRouteUpdate = () => {
+  anchorate()
+}
