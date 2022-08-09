77f382f0202d431d4823d96fac27c37f0de01ae1
Manuel Mauky <manuel.mauky@gmail.com>
2022-08-09 14:13:42 +0200

    disable google fonts

--- src/utils/typography.ts
+++ src/utils/typography.ts
@@ -2,19 +2,9 @@ import Typography from "typography"
 import theme from "typography-theme-bootstrap"
 
 theme.baseFontSize = "14px"
-theme.googleFonts = [
-  {
-    name: "Muli",
-    styles: ["600"],
-  },
-  {
-    name: "Merriweather",
-    styles: ["300", "300i", "700", "700i"],
-  },
-]
 
-theme.headerFontFamily = ["Muli", "sans-serif"]
-theme.bodyFontFamily = ["Merriweather", "Georgia", "serif"]
+theme.headerFontFamily = ["sans-serif"]
+theme.bodyFontFamily = ["Georgia", "serif"]
 
 const typography = new Typography(theme)
 
